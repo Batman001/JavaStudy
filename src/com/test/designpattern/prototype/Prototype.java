e2bbcfdfe4db9145bb02b14ddc8d280e9e3b31c4
Batman007 <sun_thinker@163.com>
2019-04-23 15:33:13 +0800

    add prototype pattern demo

--- src/com/test/designpattern/prototype/Prototype.java
+++ src/com/test/designpattern/prototype/Prototype.java
@@ -1,16 +1,42 @@
 package com.test.designpattern.prototype;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
- * @author Batman create on 2019-04-22 17:21
+ * @author Batman create on 2019-04-23 14:44
+ * 实现方法:
+ * 浅拷贝方法  调用 java.lang.Object的clone()方法
  */
-public class Prototype implements Cloneable, Serializable {
-    private static final long serialVersionUID = 1L;
-    private String string;
+public class Prototype implements Cloneable {
+    private static final long serialVersionUID = -1251595400978173322L;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private int mInt;
 
-    private SerializableObject obj;
+    public ArrayList<String> getList() {
+        return arrayList;
+    }
 
+    public int getmInt() {
+        return mInt;
+    }
 
+    public void setList(ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public void setmInt(int mInt) {
+        this.mInt = mInt;
+    }
+
+    @Override
+    public Object clone(){
+        Prototype proto = null;
+        try{
+            proto = (Prototype) super.clone();
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+        return proto;
+    }
 
 }
--- src/com/test/designpattern/prototype/SerializableObject.java
+++
@@ -1,8 +0,0 @@
-package com.test.designpattern.prototype;
-
-/**
- * @author Batman create on 2019-04-22 17:22
- */
-public class SerializableObject {
-    private static final long serialVersionUID=1L;
-}
