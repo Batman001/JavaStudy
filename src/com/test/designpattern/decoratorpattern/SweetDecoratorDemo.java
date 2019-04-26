501bacc032669493c5ae0eab8784f93eaa1f7938
Batman007 <sun_thinker@163.com>
2019-04-26 11:30:55 +0800

    add decorate design pattern demo

--- src/com/test/designpattern/decoratorpattern/SweetDecoratorDemo.java
+++ src/com/test/designpattern/decoratorpattern/SweetDecoratorDemo.java
@@ -2,6 +2,7 @@ package com.test.designpattern.decoratorpattern;
 
 /**
  * @author Batman create on 2019-04-24 14:14
+ * 装饰者模式(甜品蛋糕中添加水果或者蜡烛)
  */
 public class SweetDecoratorDemo {
     public static void main(String[] args) {
@@ -16,6 +17,13 @@ public class SweetDecoratorDemo {
 
         FruitAbstractDecorator f2 = new FruitAbstractDecorator(candleDecorator);
         System.out.println(f2.getDescription() + "总共花费" + f2.cost());
+
+        Chocolate chocolate = new Chocolate();
+        System.out.println(chocolate.getDescription() + chocolate.cost());
+
+        FruitAbstractDecorator f = new FruitAbstractDecorator(chocolate);
+        System.out.println(f.getDescription() + f.cost());
+
 
 
 
