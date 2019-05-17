abe91ef8b5e1143c3d51d6742f50fef683a0b178
Batman007 <sun_thinker@163.com>
2019-05-17 14:47:00 +0800

    更新代码细节 是代码更易读一些 分类更清晰 (基于不同的类型的适配器模式：基于类、基于对象、基于接口的模式)

--- src/com/test/reflection/Student.java
+++ src/com/test/reflection/Student.java
@@ -41,7 +41,7 @@ public class Student {
 
     }
 
-    public void getNameAndScore(String name, double score){
+    private void getNameAndScore(String name, double score){
         System.out.println("学生姓名：" + name + "\t 成绩为：" + score);
     }
 
@@ -57,23 +57,23 @@ public class Student {
         student.setName("Batman");
         student.setAge(19);
 
-        /** 1. 无参数 **/
+        /* 1. 无参数 **/
         Method method = student.getClass().getMethod("getName");
         String name = (String) method.invoke(student);
         System.out.println("通过反射调用getName方法得到student的名字为："+name);
 
-        /** 2. 一个参数 **/
+        /* 2. 一个参数 **/
         Method method1 = student.getClass().getMethod("setName", String.class);
         method1.invoke(student, "Robin");
 
-        /** 3.多个参数 */
+        /* 3.多个参数 */
         Method method2 = student.getClass().getMethod("method", int.class, String.class);
         method2.invoke(student, 88, "Joker");
 
         System.out.println(student);
 
 
-        /** 4.调用double类型的参数 **/
+        /* 4.调用double类型的参数 **/
         Method method3 = student.getClass().getMethod("getNameAndScore", String.class, double.class);
         method3.invoke(student,"Batman", 100.56);
 
